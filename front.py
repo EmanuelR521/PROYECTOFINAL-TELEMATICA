41f14e59851465ee111038b9b44a0745279bd8f1
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2023-05-02 21:02:00 -0500

    Update front.py
    .

--- front.py
+++ front.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import plotly.graph_objects as go
 import dash
-from dash import dcc
-from dash import html
+from dash import dcc, html
 from dash.dependencies import Input, Output, State
 
-
 app = dash.Dash()
-
 app.layout = html.Div([
-    html.H3("Ingresa tus credenciales:"),
+    html.H3("INICIO DE SESION:", style={'color': 'blue'}),
     dcc.Input(id="user", type="text", placeholder="Usuario"),
-    dcc.Input(id="password", type="text", placeholder="Contraseña"),
+    dcc.Input(id="password", type="password", placeholder="Contraseña"),
     html.Button('Enviar', id='submit-button', n_clicks=0),
     html.Div(id='output')
 ])
@@ -20,14 +17,28 @@ app.layout = html.Div([
               [Input('submit-button', 'n_clicks')],
               [State('user', 'value'),
                State('password', 'value')])
-
-def update_output(n_clicks, user, password):
+# en la funcion hay varios print que solo sirven para verificar hasta donde esta llegando el codigo
+def validarDatos(n_clicks, user, password):
     valido = False
-    if user == "a" and password == "a":
-      valido = True
+    ArrayUsers = []
+    ArrayPasswords = []
+    url="http://34.225.20.33:1002/recibir_DB"
+    data = pd.read_json(url,convert_dates=True)
+    print(type(data))
+    for i in range(0,3):
+      ArrayUsers.append(data['user'][i])
+      ArrayPasswords.append(data['password'][i])
+    print(ArrayUsers)
+    print(ArrayPasswords)
+    for i in range(0,3):
+      print("entra al for")
+      if ArrayUsers[i] == user and str(ArrayPasswords[i]) == password:
+        print("entra al if")
+        valido = True
+    print (valido)
 
     if valido:
-      url = "http://34.225.20.33:1001/mostrar_estacionesnivel"
+      url = "http://34.225.20.33:1001/api"
       data = pd.read_json(url,convert_dates='True')
 
       latr = []
@@ -43,13 +54,14 @@ def update_output(n_clicks, user, password):
       fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
       return html.Div([
-                 html.H1("PROYECTO API SIATA"),
+                 html.H2("BIENVENIDO AL API DEL SIATA", style ={'color': 'orange' }),
                  dcc.Graph(figure=fig)
                  ])
     else:
       if n_clicks > 0:
         return html.Div([
-            html.H3('Credenciales incorrectas', style={'color': 'red'})
-            ])
+            html.H2('USUARIO O CONTRASEÑA INCORRECTOS', style={'color': 'red'})
+        ])
+
 app.run_server(host='0.0.0.0',port=1000)
 
